490a3a90a73be9efdfb5c327ceeaa6d92c8f6c76
Tien Thanh <tienthanh100696@gmail.com>
2017-04-17 16:13:22 +0700

    1st

---
+++ app.js
@@ -0,0 +1,63 @@
+console.log('Hello nodemon');
+
+const fs = require('fs');
+//dung cai thu vien express
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const imagesController = require(__dirname + '/modules/images/imagesController');
+var app = express();
+
+//set public folder public
+//app.use(urlencoded)
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.send('./public/index.html');
+})
+
+app.post('/image', (req, res) => {
+  //doc du lieu tu file imageData
+  var imageInfoCollection = imagesController.fetchImageCollection();
+  //khai bao object
+  var imageInfo = {
+    name : req.body.name,
+    imageLink : req.body.imageLink,
+    description : req.body.description
+  }
+  //push data moi vao colletiom
+  imageInfoCollection.push(imageInfo);
+
+  //luu lai vao file
+  imagesController.saveImageCollection(imageInfoCollection);
+  //bao thanh cong
+  res.send('Success');
+})
+
+app.get('/image', (req,res) => {
+  var imageInfoCollection = imagesController.fetchImageCollection();
+
+  htmlString = '';
+
+  imageInfoCollection.forEach((data) => {
+    htmlString += `<div>${data.name}</div><img src="${data.imageLink}"><div>${data.description}</div>`;
+  })
+
+  res.send(htmlString);
+
+})
+
+app.put('/image', (req, res) => {
+
+})
+
+app.delete('/image', (req, res) => {
+  
+})
+
+//mo 1 cai port de chay local
+app.listen(6969, (req, res) => {
+  console.log('app listen on 6969');
+})
